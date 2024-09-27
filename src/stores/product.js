2489bc34c6b8c120796ca0e66a22fed21012d682
fernandapadilha <vitoriafernandaifc@gmail.com>
2024-09-27 11:46:42 -0300

    first commit

---
+++ src/stores/product.js
@@ -0,0 +1,15 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useProductStore = defineStore('product', () => {
+  const products = ref([
+    {id: 1, name: 'salgado frito', price: 4.5, qty: 100},
+    {id: 2, name: 'pão de batata', price: 5.5, qty: 50},
+    {id: 3, name: 'pão de queijo', price: 2.25, qty: 70},
+    {id: 4, name: 'suco de laranja', price: 7, qty: 1000},
+    {id: 5, name: 'Trento', price: 3, qty: 2000}
+  ])
+
+  
+  return { products }
+})
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {useProductStore} from '@/stores/product';
+
+const productStore = useProductStore();
+</script>
+
+<template>
+  <h1>listagem de produtos</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Nome</th>
+        <th>Preço</th>
+        <th>Ações</th>
+      </tr>
+    </thead> 
+    
+    <tbody>
+      <tr v-for="product in productStore.products" :key="product.id">
+        <td>{{ product.id }}</td>
+        <td>{{ product.name }}</td>
+        <td>{{ product.price }}</td>
+        <td>
+          <button>Ver</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+</template>
